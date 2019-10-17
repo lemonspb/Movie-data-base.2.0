408640e0c7a1e3f62e8b909482997c463b414faa
Misha Skopenko <misha.skopenko@yandex.ru>
2019-10-17 20:01:11 +0700

    ++

--- src/Components/FavoritePage/FavoritePage.tsx
+++ src/Components/FavoritePage/FavoritePage.tsx
@@ -4,30 +4,24 @@ import app from '../../Serviсes/base'
 
 
 export function FavoritePage() {
-const imgUrl = 'https://image.tmdb.org/t/p/w300/'
-; 
-const { currentUser } = useContext(AuthContext);
-const [listBestFilms, setListBestFilms] = useState({})
-// const starCountRef = app.database().ref('users/');
-//     if(app.auth().currentUser!==null){
-//          starCountRef.once('value',async(snapshot)=>{
-//      const listUsers = await  snapshot.val()
-//       Object.entries(listUsers[currentUser.uid].films).map((el)=> el[1])
-//      });
-// }
-function name() {
-   return  app.database().ref('users/').once('value').then(function(snapshot) {
-    const listUsers =  snapshot.val()
-  Object.entries(listUsers[currentUser.uid].films).map((el)=> el[1])
-});  
-}
-name()
-  
+    const imgUrl = 'https://image.tmdb.org/t/p/w300/'
+        ;
+    const { currentUser } = useContext(AuthContext);
+    const [listBestFilms, setListBestFilms] = useState({})
+    const starCountRef = app.database().ref('users/');
+    if (app.auth().currentUser !== null) {
+        starCountRef.once('value', async (snapshot) => {
+            const listUsers = await snapshot.val()
+            console.log(listUsers[currentUser.uid].films)
+            Object.entries(listUsers[currentUser.uid].films).map((el) => el[1])
+        });
+    }
+
     return (
         <>
-  <div>
+            <div>
 
-  </div>
+            </div>
 
         </>
     );
